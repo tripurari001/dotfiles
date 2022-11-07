005ecec0044d0c50a9c6903a950eb4670d8e51be
Tripurari Shankar <tripurari.shankar@cred.club>
2022-11-07 14:41:04 +0530

    updated to nvim 0.8 and alacritty

--- vim/lua/plugins/nvim-lspconfig.lua
+++ vim/lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ local lsp_installer = require("nvim-lsp-installer")
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
