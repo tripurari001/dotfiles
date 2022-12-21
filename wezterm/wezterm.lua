35526f918428c66941aebcb7e3d66a9b94543ec9
Tripurari Shankar <tripurari.shankar@cred.club>
2022-12-21 20:49:30 +0530

    tune line height

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require 'wezterm'
 
 return {
-  font = wezterm.font('FiraCode Nerd Font', { weight = 500 }),
+  font = wezterm.font_with_fallback {
+    { family = 'FiraCode Nerd Font', weight = 'Regular' }
+  },
   font_size = 16,
-  line_height = 1.1,
+  line_height = 1.2,
   hide_tab_bar_if_only_one_tab = true,
   color_scheme = "tokyonight-storm",
 }
