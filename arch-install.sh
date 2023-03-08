55416cf9d0f79f5fa22eec303e958c17ef883896
Tripurari Shankar <tripurari.shankar@cred.club>
2023-03-08 23:22:12 +0530

    added brightness and volume key support

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,3 @@
 sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer
 
-paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout
+paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset
