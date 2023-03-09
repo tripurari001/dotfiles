8034545a4c0246a5bff01e819a1f3fd21823d10c
Tripurari Shankar <tripurari.shankar@cred.club>
2023-03-09 11:26:27 +0530

    added sddm login config

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,3 @@
 sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer
 
-paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset
+paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset sddm-sugar-candy sddm-git
