d40b546926b06458d781a35933e16f9386bd34b4
Tripurari Shankar <tripurari.shankar@cred.club>
2023-03-12 16:43:51 +0530

    updated the hyprland config and alacritty

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,3 @@
-sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer
+sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer polkit-kde-agent pipewire wireplumber
 
 paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset sddm-sugar-candy sddm-git
