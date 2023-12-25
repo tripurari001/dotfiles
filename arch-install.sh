63bcf88890bbf95263376ac113fab2534079073f
Tripurari Shankar <tripurari.shankar@cred.club>
2023-12-25 20:30:59 +0530

    added multiple improvements

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,3 @@
-sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer polkit-kde-agent pipewire wireplumber nm-applet
+sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer polkit-kde-agent pipewire wireplumber nm-applet pacman-contrib
 
 paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset sddm-sugar-candy sddm-git ttf-material-icons-git ttf-jetbrains-mono-nerd ttf-iosevka-nerd
--- hyprland/rofi/launcher.sh
+++
@@ -1,1 +0,0 @@
-pkill rofi; rofi -no-lazy-grab -show drun -modi drun -theme ~/dotfiles/hyprland/rofi/theme.rasi
