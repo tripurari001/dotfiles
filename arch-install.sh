9f96db01ad6951360ef0502a7c2f4ac087d68d88
Tripurari Shankar <tripurari.shankar@cred.club>
2024-01-05 13:30:57 +0530

    updated package list in install script

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,3 @@
-sudo pacman -Syu waybar dunst swaylock wl-clipboard brightnessctl pamixer polkit-kde-agent pipewire wireplumber nm-applet pacman-contrib
-
-paru -Syu yofi xdg-desktop-portal-hyprland hyprland hyprpaper wlogout wlsunset sddm-sugar-candy sddm-git ttf-material-icons-git ttf-jetbrains-mono-nerd ttf-iosevka-nerd
+# install paru
+paru -Syu alacritty waybar-hyprland swaync swaylock wl-clipboard brightnessctl pipewire wireplumber polkit-gnome rofi-lbonn-wayland xdg-desktop-portal-hyprland-git hyprland-git hyprpaper wlogout sddm-git sddm-sugar-candy ttf-iosevka-nerd networkmanager nm-applet neovim qt5-wayland qt6-wayland thunar thumbler dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol
+# install asus rog control center if required
