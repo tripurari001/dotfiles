d3462c3b0a7b55f0b027d8b7947dda2610bc1052
Tripurari Shankar <tripurari.shankar@cred.club>
2024-02-02 12:25:45 +0530

    added darker varient

--- vim/lua/plugins/colors.lua
+++ vim/lua/plugins/colors.lua
@@ -1,7 +1,10 @@
 return {
     {
-        "navarasu/onedark.nvim",
+        'navarasu/onedark.nvim',
         config = function()
+            require('onedark').setup {
+                style = 'darker'
+            }
             require('onedark').load()
         end
     },
