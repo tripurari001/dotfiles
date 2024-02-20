b182260721710d0ff5271ee71755ed0a1cd29a41
Tripurari Shankar <tripurari.shankar@cred.club>
2024-02-20 09:35:07 +0530

    updated colorscheme

--- vim/lua/plugins/colors.lua
+++ vim/lua/plugins/colors.lua
@@ -6,7 +6,7 @@ return {
         opts = {},
         config = function()
             require('tokyonight')
-            vim.cmd[[colorscheme tokyonight]]
+            vim.cmd[[colorscheme tokyonight-moon]]
         end
     },
 }
