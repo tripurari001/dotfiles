01e6e10a90684402e53b3283a8eb4896315ce5da
Tripurari Shankar <tripuraris@rippling.com>
2024-06-17 21:06:35 +0530

    removed mapping for ';'

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,7 +3,3 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
-map("n", ";", ":", { desc = "CMD enter command mode" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
