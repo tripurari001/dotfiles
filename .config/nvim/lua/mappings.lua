24c9505f98400c489675a981c45c333325f1dabe
Tripurari Shankar <tripuraris@rippling.com>
2024-06-25 15:46:32 +0530

    added arglist and qlist mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,3 +3,12 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
+
+-- Square bracket mappings (many of them inspired by Unimpaired)
+map('n', '[a', '<cmd>prev<cr>', default_opts)
+map('n', ']a', '<cmd>next<cr>', default_opts)
+map('n', ']q', '<cmd>cnext<cr>zz', default_opts)
+map('n', '[q', '<cmd>cprevious<cr>zz', default_opts)
+map('n', ']Q', '<cmd>clast<cr>zz', default_opts)
+map('n', '[Q', '<cmd>cfirst<cr>zz', default_opts)
