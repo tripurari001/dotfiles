a60163cc222ebf580b737776ccdc93f809be0cf6
Tripurari Shankar <tripuraris@rippling.com>
2024-07-22 10:30:45 +0530

    added gy mapping and leader to switch butter

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -12,3 +12,23 @@ map('n', ']q', '<cmd>cnext<cr>zz', default_opts)
 map('n', '[q', '<cmd>cprevious<cr>zz', default_opts)
 map('n', ']Q', '<cmd>clast<cr>zz', default_opts)
 map('n', '[Q', '<cmd>cfirst<cr>zz', default_opts)
+
+
+-----------------------------------------------------------
+-- Easier copy/pasting to/from OS clipboard
+-----------------------------------------------------------
+map('n', 'gy', '"*y', default_opts)
+map('v', 'gy', '"*y', default_opts)
+map('n', 'gp', '"*p', default_opts)
+map('v', 'gp', '"*p', default_opts)
+map('n', 'gP', '"*P', default_opts)
+map('v', 'gP', '"*P', default_opts)
+map('n', 'gY', '"*Y', default_opts)
+
+map('n', '<leader>sp', '<cmd>set spell!<CR>', {noremap = true})
+map('n', '<c-g>', '2<c-g>', default_opts)
+map('v', '*', '"xy/<C-R>x<CR>', default_opts)
+
+-- Toggle last buffer
+map('n', '<space>', '<Nop>', default_opts) -- us
+map('n', '<leader><leader>', '<C-^>', default_opts)
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,5 +3,9 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
+local opt = vim.opt
 o.cursorlineopt = 'both' -- to enable cursorline!
 o.relativenumber = true
+
+
+opt.clipboard = ""
