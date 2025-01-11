597a9c33e258ce351f0dee190d848ff3a50a8ccb
Tripurari Shankar <tripuraris@rippling.com>
2025-01-11 19:52:46 +0530

    updated some arch and terminal config

--- arch-install.sh
+++ arch-install.sh
@@ -1,3 +1,7 @@
 # install paru
-paru -Syu alacritty waybar swaync hyprlock-git wl-clipboard brightnessctl pipewire wireplumber polkit-gnome rofi-lbonn-wayland xdg-desktop-portal-hyprland hyprland hyprpaper wlogout sddm-git sddm-sugar-candy ttf-iosevka-nerd networkmanager nm-applet neovim qt5-wayland qt6-wayland thunar gvfs thumbler dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra grim slurp
+paru -Syu ghostty waybar swaync hyprlock wl-clipboard brightnessctl pipewire wireplumber polkit-gnome rofi-wayland xdg-desktop-portal-hyprland hyprland hyprpaper wlogout sddm qt6-svg qt6-declarative qt5-quickcontrols2 sddm-sugar-candy ttf-iosevka-nerd networkmanager network-manager-applet neovim qt5-wayland qt6-wayland thunar thunar-archive-plugin ark gvfs dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra grim slurp zsh which
 # install asus rog control center if required
+# enable sddm
+#
+
+paru -Syu keepassxc ttf-firacode-nerd
