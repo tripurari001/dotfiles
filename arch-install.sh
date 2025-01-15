4723693beaeec0efa773ee0e37fb0c2d1db1e878
Tripurari Shankar <tripuraris@rippling.com>
2025-01-16 00:19:38 +0530

    updated packages and hyprland config

--- arch-install.sh
+++ arch-install.sh
@@ -1,5 +1,5 @@
 # install paru
-paru -Syu ghostty waybar swaync hyprlock wl-clipboard brightnessctl pipewire wireplumber polkit-gnome rofi-wayland xdg-desktop-portal-hyprland hyprland hyprpaper wlogout sddm qt6-svg qt6-declarative qt5-quickcontrols2 sddm-sugar-candy ttf-iosevka-nerd networkmanager network-manager-applet neovim qt5-wayland qt6-wayland thunar thunar-archive-plugin ark gvfs dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra grim slurp zsh which
+paru -Syu ghostty waybar swaync hyprlock wl-clipboard brightnessctl pipewire wireplumber hyprpolkitagent xhost rofi-wayland xdg-desktop-portal-hyprland hyprland hyprpaper wlogout sddm qt6-svg qt6-declarative qt5-quickcontrols2 sddm-sugar-candy ttf-iosevka-nerd networkmanager network-manager-applet neovim qt5-wayland qt6-wayland thunar thunar-archive-plugin ark gvfs dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra grim slurp zsh which
 # install asus rog control center if required
 # enable sddm
 #
