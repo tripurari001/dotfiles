3564221a82634830ac59e03d51070d4ddbe38c0d
Tripurari Shankar Singh <tripurari@protonmail.com>
2026-04-08 13:29:48 +0530

    removed hyprland configs

--- arch-install.sh
+++
@@ -1,7 +0,0 @@
-# install paru
-paru -Syu ghostty waybar swaync hyprlock wl-clipboard brightnessctl pipewire wireplumber hyprpolkitagent xhost rofi-wayland xdg-desktop-portal-hyprland hyprland hyprpaper wlogout sddm qt6-svg qt6-declarative qt5-quickcontrols2 sddm-sugar-candy ttf-iosevka-nerd networkmanager network-manager-applet neovim qt5-wayland qt6-wayland thunar thunar-archive-plugin ark gvfs dracula-gtk-theme dracula-icons-git less man-db imv plymouth plymouth-theme-dark-arch lxappearance xfce4-settings pavucontrol noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra grim slurp zsh which
-# install asus rog control center if required
-# enable sddm
-#
-
-paru -Syu keepassxc ttf-firacode-nerd
